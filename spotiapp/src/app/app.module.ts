61d8eb4b0304bcf330e71d3c40a9e9a81814c80b
Tomas Corral Casas <tomascorralcasas@Tomas-Corral-Casass-MacBook-Pro.local>
2018-12-18 21:30:24 +0100

    Creating page and shared components

--- spotiapp/src/app/app.module.ts
+++ spotiapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SharedComponent } from './components/shared/shared.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SharedComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
